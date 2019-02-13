2eba65e7a81a3a6ce3df3f48a837343b665fa8b1
Christian-Ivanov <Christian-Ivanov>
2019-02-13 12:35:29 +0200

    ch merge

--- source.c
+++ source.c
@@ -8,11 +8,8 @@ int main(){
     int c=221221;
 
     printf("a = " + a + ", b = " + b);
-<<<<<<< HEAD
     
-=======
     int kuku;
->>>>>>> 0e0c54b69aa9e2174173e79386c7bf44353e09d4
 
     return 0;
 }
